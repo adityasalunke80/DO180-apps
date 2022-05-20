d164aaa4875492b4ef18401508120126dbce4039
Student User <student@workstation.lab.example.com>
2022-05-20 02:30:51 -0400

    updated app

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,8 @@
+<?ndex.php
 <?php
+
+print "Hello, World! php version is " . PHP_VERSION . "\n";
+
+print "A change is in the air!\n"php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 ?>
